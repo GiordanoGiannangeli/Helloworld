957dbc4020e4299d64ece48b9ef134f9c3a86f6b
giordy <giordy@DESKTOP-MOQ5PU9.homenet.telecomitalia.it>
2024-11-29 17:09:41 +0100

    primo commit

---
+++ HelloWorld/src/main/HelloWorld.java
@@ -0,0 +1,18 @@
+package main;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		
+		System.out.print("Ciao Giordano");
+		System.out.print(5);
+		System.out.print(3);
+		//ciao
+		/*ciao
+		 * c
+		 * i
+		 * a
+		 * o
+		 */
+	}
+
+}
